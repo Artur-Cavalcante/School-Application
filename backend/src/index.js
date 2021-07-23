4d3258e7c466f05175ebe6d2f2b278f125d019fa
Artur Cavalcante <arturcavalcante2014.p@hotmail.com>
2021-07-22 22:20:11 -0300

    reajuste index

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,34 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-const { Client } = require("pg");
 
 const dotenv = require("dotenv");
-dotenv.config();
 
-//const routes = require('./routes');
+const { routes } = require("@/routes");
 
 const PORT = process.env.PORT || 3333;
 
+dotenv.config();
+
 const app = express();
-
-const client = new Client({
-  host: "batyr.db.elephantsql.com",
-  user: "cnzmqvzx",
-  database: "cnzmqvzx",
-  password: "hvrDvsq1NfLxirLD3FZLhLmmhfhOQFhh",
-  port: 5432,
-});
-
-client.connect();
-
-client.query("SELECT * FROM titulo", (err, res) => {
-  console.log(err, res.rows);
-  client.end();
-});
 
 app.use(express.json());
 app.use(cors());
-app.get("/api", (request, response) => {
-  response.json("Hello World!");
-});
+app.use(routes);
+
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
