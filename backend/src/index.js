b5295f8569243cf35471db46b44d1e22bd1be1bb
Artur Cavalcante <arturcavalcante2014.p@hotmail.com>
2021-07-24 17:29:52 -0300

    initing config typescript

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 
 const dotenv = require("dotenv");
 
-const { routes } = require("@/routes");
+const { routes } = require("./routes/index");
 
 const PORT = process.env.PORT || 3333;
 
@@ -14,5 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(routes);
+
+require("@/infra/context/index");
 
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
